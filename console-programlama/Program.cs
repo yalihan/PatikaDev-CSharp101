8f163902ba045f9cec2c8a258117901ced0cc290
Alihan Yılmaz <coderyilmaz@gmail.com>
2021-09-30 21:57:05 +0300

    Add project files.

---
+++ console-programlama/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi Giriniz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Giriniz: ");
+            string surName = Console.ReadLine();
+            Console.WriteLine("Hoşgeldiniz " + name + " " + surName);
+        }
+    }
+}
