5cb2b5663c25e57397f49584824e7bbedc293fbb
Alihan Yılmaz <coderyilmaz@gmail.com>
2021-10-01 19:59:33 +0300

    Added, IfElseIf

---
+++ IfElseIf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IfElseIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int simdikiZaman = DateTime.Now.Hour;
+            Console.WriteLine(simdikiZaman);
+
+            string sonuc;
+            if (simdikiZaman >= 6 && simdikiZaman < 11)
+                sonuc = "Günaydın";
+            else if (simdikiZaman < 18)
+                sonuc = "İyi Günler";
+            else
+                sonuc = "İyi Akşamlar";
+
+            string sonucTernary;
+
+            sonucTernary =
+                simdikiZaman < 11 ? "Günaydın" : 
+                simdikiZaman <= 18 ? "İyi Günler" : "İyi Akşamlar";
+
+            Console.WriteLine("Normal ifElse ifadesinin sonucu : " + sonuc);
+            Console.WriteLine("Ternary ifadesinin sonucu : " + sonucTernary);
+        }
+    }
+}
