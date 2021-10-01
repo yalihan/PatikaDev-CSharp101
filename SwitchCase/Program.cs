c08e26d629ea428ab3583aba3650a6e341a6fcbd
Alihan Yılmaz <coderyilmaz@gmail.com>
2021-10-01 20:52:17 +0300

    Added, SwitchCase

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int simdikiAy = DateTime.Now.Month;
+
+            switch (simdikiAy) //Expression
+            {
+                case 1:
+                    Console.WriteLine("Ocak");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayıs");
+                    break;
+                case 6:
+                    Console.WriteLine("Haziran");
+                    break;
+                case 7:
+                    Console.WriteLine("Temmuz");
+                    break;
+                case 8:
+                    Console.WriteLine("Ağustos");
+                    break;
+                case 9:
+                    Console.WriteLine("Eylül");
+                    break;
+                case 10:
+                    Console.WriteLine("Ekim");
+                    break;
+                case 11:
+                    Console.WriteLine("Kasım");
+                    break;
+                case 12:
+                    Console.WriteLine("Aralık");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış Veri Girdiniz!");
+                    break;
+            }
+            switch (simdikiAy)
+            {
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar");
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
